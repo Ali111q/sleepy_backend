ca7eabead6cb1836e3ac437f738fb55439510777
Ali111q <93655964+Ali111q@users.noreply.github.com>
2024-07-17 11:51:07 +0300

    main

--- Moi/Controllers/NotificationsController.cs
+++ Moi/Controllers/NotificationsController.cs
@@ -1,12 +1,16 @@
 using GaragesStructure.DATA.DTOs.Notification;
 using GaragesStructure.Entities;
+using GaragesStructure.Helpers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace GaragesStructure.Controllers;
-[ServiceFilter(typeof(AuthorizeFilter))]
+[ServiceFilter(typeof(AuthorizeActionFilter))]
+
 public class NotificationsController: BaseController
 {
+    [Authorize]
     [HttpPost]
     public async Task<ActionResult<NotificationResponse>> SendNotification([FromBody] NotificationForm form) =>
         Ok(new NotificationResponse(State: true, Notification: new Notifications()));
